7fa7e9fcb29cb45474ea55fc4beaf3f768a44f89
Ayush331-coder <ayushrandive545@gmail.com>
2025-02-07 21:14:48 +0530

    Temples

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -58,6 +58,7 @@ export const signin = async (req, res, next) => {
 }
 
 export const google = async (req, res, next) =>{
+  console.log("Received Google User Data:",req.body);
   const {email, name, googlePhotoUrl} = req.body;
   try {
     const user = await User.findOne({email});
@@ -77,11 +78,15 @@ export const google = async (req, res, next) =>{
      profilePicture: googlePhotoUrl,
   });
    await newUser.save();
-   const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
-    const {password, ...rest} = user._doc;
-    res.status(200).cookie('access_token',token,{
+   console.log("New Google User stored", newUser);
+   const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET);
+    const {password, ...rest} = newUser._doc;
+    res
+    .status(200)
+    .cookie('access_token',token,{
       httpOnly: true,
-    }).json(rest);
+    })
+    .json(rest);
   }
   
  } catch (error) {
